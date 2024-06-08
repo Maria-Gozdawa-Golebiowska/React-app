972c379a6bde116c7a08674769517229cdda051e
Maria Gozdawa-Gołębiowska <m.gozdawa-golebiowska@gmail.com>
2024-06-08 17:13:17 +0200

    react test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Hero from './components/Hero/Hero';
+import SearchForm from './components/SearchForm/SearchForm';
+import List from './components/List/List.js'
+import Container from './components/Container/Container.js';
+
+const App = () => {
+  return (
+    <Container>
+      <Hero />
+      <SearchForm />
+      <List />
+    </Container>
+    
+  );
+};
+
+export default App;
+
